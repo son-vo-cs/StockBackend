e33b53efe494b3e6a2ea05c304556e915eebe5ed
son-vo-cs <sonvo.cs@gmail.com>
2020-05-21 19:51:18 -0700

    updates

--- src/main/java/com/example/demo/repository/CountryRepository.java
+++ src/main/java/com/example/demo/repository/CountryRepository.java
@@ -3,11 +3,14 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import com.example.demo.model.Country;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long>{
-	
+	@Query("SELECT countryname, population FROM Country coun where coun.countryname = :name "
+			+ "AND coun.population = :population" )
+	String findTitleById(@Param("name") String name, @Param("population") Long population);
 
 }
