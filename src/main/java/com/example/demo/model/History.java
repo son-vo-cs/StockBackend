5c5544a71454ec74bd91945c9b70cddea01ef472
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 01:07:46 -0700

    updates

--- src/main/java/com/example/demo/model/History.java
+++ src/main/java/com/example/demo/model/History.java
@@ -5,4 +5,19 @@ import javax.persistence.*;
 @Table(name = "history")
 public class History {
 	
+	private String id, symbol, date;
+	private int change;
+	public History() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public History(String id, String symbol, String date, int change) {
+		super();
+		this.id = id;
+		this.symbol = symbol;
+		this.date = date;
+		this.change = change;
+	}
+	
+	
 }
