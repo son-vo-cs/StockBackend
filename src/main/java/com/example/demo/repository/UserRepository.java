2bdabe328b9b0fad72e41e3af3e39cbf1ccc412c
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 01:20:15 -0700

    updates

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
-import com.example.demo.model.Country;
+import com.example.demo.model.AppUser;
 import org.springframework.data.repository.query.Param;
 
 @Repository
-public interface UserRepository extends JpaRepository<Country, Long>{
+public interface UserRepository extends JpaRepository<AppUser, Long>{
 	
 }
