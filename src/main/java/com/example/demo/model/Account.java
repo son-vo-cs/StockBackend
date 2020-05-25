d171f80c4275b3c9c9fecf6a99a326427bd10173
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 01:04:46 -0700

    updates

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -13,6 +13,30 @@ public class Account {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	public Account(long id, int shares, String symbol) {
+		super();
+		this.id = id;
+		this.shares = shares;
+		this.symbol = symbol;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getShares() {
+		return shares;
+	}
+	public void setShares(int shares) {
+		this.shares = shares;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
 	
 	
 
