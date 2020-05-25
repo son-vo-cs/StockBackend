662607cbd7052142cafd387cbdc31516f7e4a893
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 01:10:01 -0700

    updates

--- src/main/java/com/example/demo/model/Perform.java
+++ src/main/java/com/example/demo/model/Perform.java
@@ -5,5 +5,14 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "perform")
 public class Perform {
-
+	
+	private long id;
+	private String date;
+	private float price;
+	public Perform() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
