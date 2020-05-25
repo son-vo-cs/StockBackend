0507648d79b950857e9f6b60e1d265a837d4bde9
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 00:58:39 -0700

    updates

--- src/main/java/com/example/demo/model/Card.java
+++ src/main/java/com/example/demo/model/Card.java
@@ -1,5 +1,17 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 public class Card {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long cid;
+	
+	private long uid;
+	private String name, expire;
+	
 }
