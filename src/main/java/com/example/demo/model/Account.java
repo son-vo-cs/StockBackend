929db11260cad75841a9c207806f2c02017130f3
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 01:04:20 -0700

    updates

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -5,5 +5,15 @@ import javax.persistence.*;
 @Entity
 @Table(name = "account")
 public class Account {
+	
+	private long id;
+	private int shares;
+	private String symbol;
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
