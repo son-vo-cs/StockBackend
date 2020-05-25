21d1cd0c61e7ec2136e251e0213699b3289cc15e
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 00:59:37 -0700

    updates

--- src/main/java/com/example/demo/model/Card.java
+++ src/main/java/com/example/demo/model/Card.java
@@ -13,5 +13,50 @@ public class Card {
 	
 	private long uid;
 	private String name, expire;
+	public Card() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Card(long cid, long uid, String name, String expire) {
+		super();
+		this.cid = cid;
+		this.uid = uid;
+		this.name = name;
+		this.expire = expire;
+	}
+	
+	public long getCid() {
+		return cid;
+	}
+	
+	public void setCid(long cid) {
+		this.cid = cid;
+	}
+	
+	public long getUid() {
+		return uid;
+	}
+	
+	public void setUid(long uid) {
+		this.uid = uid;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getExpire() {
+		return expire;
+	}
+	
+	public void setExpire(String expire) {
+		this.expire = expire;
+	}
+	
+	
 	
 }
