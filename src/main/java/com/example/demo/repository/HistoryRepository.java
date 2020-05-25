e5d33a71a3eec17bfcca5ee99213867d25abf3d6
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 01:21:11 -0700

    updates

--- src/main/java/com/example/demo/repository/HistoryRepository.java
+++ src/main/java/com/example/demo/repository/HistoryRepository.java
@@ -1,5 +1,15 @@
 package com.example.demo.repository;
 
-public class HistoryRepository {
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import com.example.demo.model.History;
+import org.springframework.data.repository.query.Param;
+
+@Repository
+public interface HistoryRepository extends JpaRepository<History, Long>{
 
 }
