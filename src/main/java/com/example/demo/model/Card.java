70e09dd9999d8dfde00a4c186e589545f7d1b40d
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 01:00:58 -0700

    updates

--- src/main/java/com/example/demo/model/Card.java
+++ src/main/java/com/example/demo/model/Card.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+@Entity
+@Table(name = "card")
 public class Card {
 	
 	@Id
