a9b373aeb9c23de90dcfd47e517f21006fe3e224
son-vo-cs <sonvo.cs@gmail.com>
2020-05-25 23:16:49 -0700

    updates

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,9 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import com.example.demo.model.AppUser;
+import com.example.demo.model.Country;
+
 import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long>{
-	
+	@Query("SELECT coun FROM Country coun where coun.population >= :population")
+	List<Country> findCountryByNameVal(@Param("population") Long population);
 }
+
+
+/*
+ * 
+ * check if email + pass is correct
+	add fund
+	subtract fund
+	add deposit
+	withdraw
+*/
