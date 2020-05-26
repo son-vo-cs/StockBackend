113f590573f9e0712de60a8080acf341340aa105
son-vo-cs <sonvo.cs@gmail.com>
2020-05-26 00:59:27 -0700

    updates

--- src/main/java/com/example/demo/model/Perform.java
+++ src/main/java/com/example/demo/model/Perform.java
@@ -1,11 +1,17 @@
 package com.example.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
 @Table(name = "perform")
 public class Perform {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long fid;
 	private long id;
 	private String date;
 	private float price;
