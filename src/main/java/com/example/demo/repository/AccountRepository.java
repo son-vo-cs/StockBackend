ffe09aa03733f36f0997d6d0b5954a04ba4704c0
son-vo-cs <sonvo.cs@gmail.com>
2020-05-26 21:54:06 -0700

    updates

--- src/main/java/com/example/demo/repository/AccountRepository.java
+++ src/main/java/com/example/demo/repository/AccountRepository.java
@@ -4,12 +4,25 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import com.example.demo.model.Account;
+import com.example.demo.model.Card;
+
 import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
-
+	@Query("SELECT account FROM Account account where account.id = :id")
+	List<Card> getStocks(@Param("id") Long id);
+	
+	@Query("DELETE FROM Account account where account.id = :id AND account.symbol = :symbol")
+	void remove(@Param("id") Long id, @Param("symbol") String symbol);
+	
+	
+	@Modifying
+	@Query("update Account account set account.shares = :shares where account.id = :id AND account.symbol = :symbol")
+	void setFun(@Param("fund") float fund, @Param("id") Long id, @Param("symbol") String symbol);
+	
 }
