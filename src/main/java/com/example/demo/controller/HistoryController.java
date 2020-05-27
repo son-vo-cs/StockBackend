8f422e4e03b22bca72ee180decfa9d9b9d3c91fd
son-vo-cs <sonvo.cs@gmail.com>
2020-05-27 00:42:39 -0700

    updates

--- src/main/java/com/example/demo/controller/HistoryController.java
+++ src/main/java/com/example/demo/controller/HistoryController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.http.HttpStatus;
 
-import com.example.demo.model.Country;
 import com.example.demo.model.History;
 import com.example.demo.repository.HistoryRepository;
 import com.example.demo.exception.*;
