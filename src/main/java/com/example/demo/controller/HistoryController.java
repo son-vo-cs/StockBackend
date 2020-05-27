b8e7982c261d9589e9f9974a66bd97eec6b2d509
son-vo-cs <sonvo.cs@gmail.com>
2020-05-26 23:55:04 -0700

    updates

--- src/main/java/com/example/demo/controller/HistoryController.java
+++ src/main/java/com/example/demo/controller/HistoryController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.http.HttpStatus;
+import com.example.demo.model.History;
+import com.example.demo.repository.HistoryRepository;
+import com.example.demo.exception.*;
+
+@RestController
+@RequestMapping("/")
 public class HistoryController {
 
 }
--- src/main/java/com/example/demo/repository/HistoryRepository.java
+++ src/main/java/com/example/demo/repository/HistoryRepository.java
@@ -4,12 +4,20 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.model.Account;
 import com.example.demo.model.History;
 import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Long>{
-
+	
+	@Modifying
+	@Query("DELETE FROM History his where his.id = :id AND account.symbol = :symbol")
+	void remove(@Param("id") Long id, @Param("symbol") String symbol);
+	
+	
 }
