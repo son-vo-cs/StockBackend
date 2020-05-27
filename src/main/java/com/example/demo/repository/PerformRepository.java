181c22d4200f96343656d26fa0ba2c56ff3b52df
son-vo-cs <sonvo.cs@gmail.com>
2020-05-27 01:25:42 -0700

    updates

--- src/main/java/com/example/demo/repository/PerformRepository.java
+++ src/main/java/com/example/demo/repository/PerformRepository.java
@@ -6,10 +6,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.model.Account;
 import com.example.demo.model.Perform;
 import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface PerformRepository extends JpaRepository<Perform, Long>{
-
+	@Query("SELECT perform FROM Perform perform where perform.id = :id")
+	List<Account> getPerform(@Param("id") Long id);
 }
